e0d03387eec7950a05f6f37fb2a4664e48a284d6
SivCloud <11730168+NQPhuc@users.noreply.github.com>
2021-06-06 11:41:36 +0700

    add axios

--- packages/simple-calculator-front/src/views/Calculate.vue
+++ packages/simple-calculator-front/src/views/Calculate.vue
@@ -1,24 +1,16 @@
 <template>
-  <div className="App">
-      <p>integer 1: </p> <textarea id="a" rows="6" cols="100"></textarea>
-      <button type="button">Reset</button>
-
-      <p>integer 2: </p> <textarea id="b" rows="6" cols="100"></textarea>
-      <button type="button">Reset</button>
-
-      <p>
-        operator: 
-        <select id="op">
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="%">%</option>
-        </select><br/>
-      </p>
-      
-      <button type="button">Calculate</button>
-
-      <p>Result: </p> <textarea id="result" rows="10" cols="100" disabled></textarea>
-
-    </div>
+  <div>
+    <p>CALCULATOR PAGE</p>
+    <CalculatorComponent />
+  </div>
 </template>
+<script>
+import * as http from "../services";
+import CalculatorComponent from "@/components/Calculator.vue";
+export default{
+  name: "Calculate",
+  components: {
+    CalculatorComponent
+  }
+}
+</script>
